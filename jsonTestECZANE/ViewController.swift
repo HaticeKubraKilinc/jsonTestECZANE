d65ca3730717a10efa69e4d8418f0bdc35c8b558
HaticeKubraKilinc <htckbr_@outlook.com>
2018-04-12 10:04:44 +0300

    Initial Commit

---
+++ jsonTestECZANE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsonTestECZANE
+//
+//  Created by hatice kübra kılınç on 12.04.2018.
+//  Copyright © 2018 hatice kübra kılınç. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
